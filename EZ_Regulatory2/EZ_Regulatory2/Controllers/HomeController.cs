1a794164facc603ac8cc22d9bc76ced925541624
Diarmuid O'Keeffe <iamdermo@gmail.com>
2013-07-03 18:28:16 +0100

    Basic Project Added

---
+++ EZ_Regulatory2/EZ_Regulatory2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EZ_Regulatory2.Models;
+
+namespace EZ_Regulatory2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private MyModelDBContext db = new MyModelDBContext();
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Check the status of the Monthly Checklists here.";
+            List<Survey> surveys = db.Surveys.ToList();
+            List<Survey> filteredList = new List<Survey>();
+            foreach(Survey s in surveys) {
+                if(s.Month.ToUpper() == "JULY") {
+                    filteredList.Add(s);
+                } else if (s.Approved.ToUpper() == "NO")
+                {
+                    filteredList.Add(s);
+                }
+            }
+            return View(filteredList);
+        }
+
+        public ActionResult Surveys()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            ViewBag.Message = "Your admin page.";
+
+            return View();
+        }
+    }
+}
---
+++ EZ_Regulatory2/EZ_Regulatory2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EZ_Regulatory2.MvcApplication" Language="C#" %>
