6b2f3cc68a8bc4071f63c608f8d2ffc285aae3e6
Diarmuid O'Keeffe <iamdermo@gmail.com>
2013-07-30 00:05:47 +0100

    Export to CSV added

--- EZ_Regulatory3/EZ_Regulatory3/Controllers/ManagerChecklistController.cs
+++ EZ_Regulatory3/EZ_Regulatory3/Controllers/ManagerChecklistController.cs
@@ -7,6 +7,8 @@ using System.Web;
 using System.Web.Mvc;
 using EZ_Regulatory3.Models;
 using EZ_Regulatory3.ViewModels;
+using EZ_Regulatory3.CSV;
+using System.Data.OleDb;
 
 namespace EZ_Regulatory3.Controllers
 {
@@ -171,6 +173,72 @@ namespace EZ_Regulatory3.Controllers
             db.Users.Remove(user);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult CSV(int userID, int surveyID)
+        {
+            SurveyAnswer surveyAnswer = db.SurveyAnswers.ToList()
+                .Where(i => i.SurveyID == surveyID && i.UserID == userID)
+                .Single();
+
+            Survey survey = db.Surveys.ToList()
+                .Where(i => i.ID == surveyID)
+                .Single();
+
+            User user = db.Users.ToList()
+                .Where(i => i.ID == userID)
+                .Single();
+
+            DataTable dt = new DataTable(); 
+            DataColumn cl = new DataColumn(survey.Title); 
+            dt.Columns.Add(cl); //--Add 'No' Column in datatable 
+            cl = new DataColumn(" "); //-- Create column 'Name'
+            dt.Columns.Add(cl);
+            cl = new DataColumn("Month:"); //-- Create column 'Name'
+            dt.Columns.Add(cl);
+            cl = new DataColumn(survey.Month); //-- Create column 'Name'
+            dt.Columns.Add(cl); //--Add 'Name' Column in datatable
+            cl = new DataColumn("  "); //-- Create column 'Name'
+            dt.Columns.Add(cl); //--Add 'Name' Column in datatable
+            cl = new DataColumn("User:"); //-- Create column 'Name'
+            dt.Columns.Add(cl);
+            cl = new DataColumn(user.Name); //-- Create column 'Name'
+            dt.Columns.Add(cl);
+
+            DataRow dr = dt.NewRow(); //-- get new row for datatable.
+            dr[0] = "You or people reporting to you have made commitments to adhering to company policy on Risk and Compliance, including providing this checklist to Risk and Compliance by the due date"; //-- assign value for 'No' column
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow(); //-- get new row for datatable
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow(); //-- get new row for datatable.
+            dr[0] = "Nature of Question"; //-- assign value for 'No' column
+            dr[1] = "Question Number:";
+            dr[2] = "Question";
+            dr[3] = "Did you adhere to this requirement?";
+            dr[4] = "Where you answer No please explain";
+            dt.Rows.Add(dr);
+
+
+            //-- add first row in datatable
+            dr = dt.NewRow(); 
+            dt.Rows.Add(dr);  // Add row in datatable
+
+
+            //-- add second row in datatable
+            foreach (Answer ans in surveyAnswer.Answers)
+            {
+                Question question = db.Questions.Find(ans.QuestionID);
+                dr = dt.NewRow();
+                dr[0] = "";
+                dr[1] = question.QuestionID;
+                dr[2] = question.Title;
+                dr[3] = ans.QuestionAnswer;
+                dr[4] = ans.QuestionComment;
+                dt.Rows.Add(dr);
+            }
+            return new CsvActionResult(dt) { FileDownloadName = "ExportedFileName.csv" };
         }
 
         protected override void Dispose(bool disposing)
