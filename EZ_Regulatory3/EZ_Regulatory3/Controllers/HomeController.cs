1ac2a75e76c9698f74879410becabb384c9c873f
Bazkelly <bazkelly@gmail.com>
2013-08-01 11:44:29 +0100

    Changed Home page text header
    Removed MVC blurb

--- EZ_Regulatory3/EZ_Regulatory3/Controllers/HomeController.cs
+++ EZ_Regulatory3/EZ_Regulatory3/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace EZ_Regulatory3.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Welcome to EZ Reg";
 
             return View();
         }
